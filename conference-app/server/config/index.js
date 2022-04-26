74dae1159eafc1ef5872f7be450d29774beeffa4
SantiagoBedoya <a.sbedoya200110@gmail.com>
2022-04-26 00:04:21 -0500

    chore(): node.js microservices

---
+++ conference-app/server/config/index.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  development: {
+    sitename: 'Roux Meetups [Development]',
+    serviceRegistryURL: 'http://localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
+  },
+  production: {
+    sitename: 'Roux Meetups',
+    serviceRegistryURL: 'http://localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
+  },
+};
